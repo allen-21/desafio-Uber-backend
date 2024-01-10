5673b55e687e99718be7595ba3b125fbb5093456
Anibal Valter Muchanga <valteranibal1@gmail.com>
2024-01-10 12:23:09 +0200

    implementa o core da aplicacao

--- src/main/java/com/muchanga/emailservice/application/EmailSenderService.java
+++ src/main/java/com/muchanga/emailservice/application/EmailSenderService.java
@@ -1,4 +1,17 @@
 package com.muchanga.emailservice.application;
 
-public class EmailSenderService {
+import com.muchanga.emailservice.adapters.EmailSenderGeteway;
+import com.muchanga.emailservice.core.EmailSenderUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EmailSenderService implements EmailSenderUseCase {
+    private final EmailSenderGeteway emailSenderGeteway;
+    @Autowired
+    public EmailSenderService(EmailSenderGeteway emailGeteway){
+        this.emailSenderGeteway = emailGeteway;
+    }
+    @Override
+    public void sendeEmail(String to, String subject, String body) {
+
+    }
 }
