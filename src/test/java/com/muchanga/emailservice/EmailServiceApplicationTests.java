f7a1e64c57b594635ac5e7d8ede1482f08545086
Anibal Valter Muchanga <valteranibal1@gmail.com>
2024-01-10 11:55:29 +0200

    criando o projeto

---
+++ src/test/java/com/muchanga/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muchanga.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
